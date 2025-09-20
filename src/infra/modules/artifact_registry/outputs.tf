b11ebc59d8d5b6f969f75e912ea399f4973a3bc2
AdnRamos <aderamos108@gmail.com>
2025-09-20 14:02:25 -0300

    add config artifact registry

---
+++ src/infra/modules/artifact_registry/outputs.tf
@@ -0,0 +1,3 @@
+output "repository" {
+  value = google_artifact_registry_repository.docker.name
+}
---
+++ src/infra/modules/artifact_registry/variables.tf
@@ -0,0 +1,3 @@
+variable "project_id" { type = string }
+variable "region"     { type = string }
+variable "repo_id"    { type = string }  # ex: "apps"
