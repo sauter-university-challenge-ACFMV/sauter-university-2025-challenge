5a1e36b2716ad3c62f66a3b6f7527b65f55fcddf
Clauderson Branco Xavier <xavierclauderson98@gmail.com>
2025-09-21 17:33:44 -0300

    feat: adição de graficos e alertas de saude da api

--- src/infra/modules/monitoring/main.tf
+++ src/infra/modules/monitoring/main.tf
@@ -1,3 +1,56 @@
+resource "google_monitoring_alert_policy" "error_5xx_alert" {
+  display_name = "API 5xx Error Rate Alert"
+  combiner     = "OR"
+  enabled      = true
+  notification_channels = [google_monitoring_notification_channel.budget_email.id]
+
+  conditions {
+    display_name = "5xx Errors > 0"
+    condition_threshold {
+      filter = "metric.type=\"run.googleapis.com/request/count\" resource.type=\"cloud_run_revision\" resource.label.service_name=\"${var.service_name}\" metric.label.response_code_class=\"5xx\""
+      aggregations {
+        alignment_period     = "60s"
+        per_series_aligner   = "ALIGN_RATE"
+      }
+      comparison      = "COMPARISON_GT"
+      threshold_value = 0
+      duration        = "300s" # 5 minutos
+      trigger {
+        count = 1
+      }
+    }
+  }
+
+  alert_strategy {
+    auto_close = "300s"
+  }
+}
+resource "google_monitoring_alert_policy" "latency_alert" {
+  display_name = "API Latency Alert"
+  combiner     = "OR"
+  enabled      = true
+  notification_channels = [google_monitoring_notification_channel.budget_email.id]
+
+  conditions {
+    display_name = "High Latency (p95 > 500ms)"
+    condition_threshold {
+      filter          = "metric.type=\"run.googleapis.com/request/latencies\" resource.type=\"cloud_run_revision\" resource.label.service_name=\"${var.service_name}\""
+      aggregations {
+        alignment_period     = "60s"
+        per_series_aligner   = "ALIGN_PERCENTILE_95"
+      }
+      comparison      = "COMPARISON_GT"
+      threshold_value = 500000000
+      duration        = "300s" 
+      trigger {
+        count = 1
+      }
+    }
+  }
+  alert_strategy {
+    auto_close = "300s" 
+  }
+}
 resource "google_monitoring_notification_channel" "budget_email" {
   project      = var.project_id
   display_name = "Budget Alerts"
@@ -33,6 +86,81 @@ resource "google_monitoring_dashboard" "api_health" {
             }]
             yAxis = {
               label = "Latency (ms)"
+            }
+          }
+        }
+      }, {
+        width  = 12
+        height = 4
+        yPos   = 4
+        widget = {
+          title = "API Error Rate (5xx)"
+          xyChart = {
+            dataSets = [{
+              timeSeriesQuery = {
+                timeSeriesFilter = {
+                  filter = "metric.type=\"run.googleapis.com/request/count\" resource.type=\"cloud_run_revision\" resource.label.service_name=\"${var.service_name}\" metric.label.response_code_class=\"5xx\""
+                  aggregation = {
+                    alignmentPeriod    = "60s"
+                    perSeriesAligner   = "ALIGN_RATE"
+                    crossSeriesReducer = "REDUCE_SUM"
+                  }
+                }
+              }
+              plotType = "LINE"
+            }]
+            yAxis = {
+              label = "Errors per second"
+            }
+          }
+        }
+      }, {
+        width  = 12
+        height = 4
+        yPos   = 8
+        widget = {
+          title = "API Queries per Second (QPS)"
+          xyChart = {
+            dataSets = [{
+              timeSeriesQuery = {
+                timeSeriesFilter = {
+                  filter = "metric.type=\"run.googleapis.com/request/count\" resource.type=\"cloud_run_revision\" resource.label.service_name=\"${var.service_name}\""
+                  aggregation = {
+                    alignmentPeriod    = "60s"
+                    perSeriesAligner   = "ALIGN_RATE"
+                    crossSeriesReducer = "REDUCE_SUM"
+                  }
+                }
+              }
+              plotType = "LINE"
+            }]
+            yAxis = {
+              label = "Requests per second"
+            }
+          }
+        }
+      }, {
+        width  = 12
+        height = 4
+        yPos   = 12
+        widget = {
+          title = "Daily Cost"
+          xyChart = {
+            dataSets = [{
+              timeSeriesQuery = {
+                timeSeriesFilter = {
+                  filter = "metric.type=\"billing.googleapis.com/billing_account/current_month/cost\" resource.type=\"cloud_run_revision\" metric.label.currency=\"BRL\" resource.label.service_name=\"${var.service_name}\""
+                  aggregation = {
+                    alignmentPeriod    = "86400s"
+                    perSeriesAligner   = "ALIGN_SUM"
+                    crossSeriesReducer = "REDUCE_SUM"
+                  }
+                }
+              }
+              plotType = "LINE"
+            }]
+            yAxis = {
+              label = "BRL per day"
             }
           }
         }
