e6d088b4bdb68f4b568be8f1edd858e85c325943
Raylandson <www.raylandsoncs@gmail.com>
2025-09-24 14:12:47 -0300

    feat: adicionando nova database

--- src/infra/modules/bigquery/variables.tf
+++ src/infra/modules/bigquery/variables.tf
@@ -3,5 +3,5 @@ variable "location"   { type = string }   # ex.: southamerica-east1
 variable "env"        { type = string }   # ex.: dev
 variable "datasets" {
   type    = list(string)
-  default = ["bronze", "silver", "gold"]
+  default = ["bronze", "silver", "gold","procedure"]
 }
